500c1fd69ebd94a434a7079c4166d3a9b1fb7407
Pascal Sochacki <pascal@sochacki.dev>
2025-12-28 23:43:17 +0100

    add types

---
+++ types.go
@@ -0,0 +1,183 @@
+package database
+
+import (
+	"encoding/binary"
+	"fmt"
+)
+
+type DataType uint32
+
+const (
+	TYPE_BYTES DataType = 1 // string (arbitrary bytes)
+	TYPE_INT64 DataType = 2 // integer (64-bit signed)
+)
+
+type Value struct {
+	Type DataType
+	I64  int64
+	Str  []byte
+}
+
+func (v Value) encode() []byte {
+	b := []byte{}
+	b = binary.LittleEndian.AppendUint32(b, uint32(v.Type))
+	switch v.Type {
+	case TYPE_INT64:
+		b = binary.LittleEndian.AppendUint64(b, uint64(v.I64))
+	case TYPE_BYTES:
+		b = binary.LittleEndian.AppendUint16(b, uint16(len(v.Str)))
+		b = append(b, v.Str...)
+	}
+	return b
+}
+
+// Value constructors
+func NewInt64(val int64) Value {
+	return Value{
+		Type: TYPE_INT64,
+		I64:  val,
+	}
+}
+
+func NewBytes(val []byte) Value {
+	return Value{
+		Type: TYPE_BYTES,
+		Str:  val,
+	}
+}
+
+type Column struct {
+	Name string
+	Type DataType
+}
+
+type Record struct {
+	values map[string]Value
+}
+
+// Record methods
+func NewRecord() Record {
+	return Record{
+		values: map[string]Value{},
+	}
+
+}
+func (r *Record) AddStr(col string, val []byte) {
+	r.values[col] = NewBytes(val)
+}
+
+func (r *Record) AddInt64(col string, val int64) {
+	r.values[col] = NewInt64(val)
+}
+
+func (r *Record) Get(col string) (Value, bool) {
+	v, ok := r.values[col]
+	return v, ok
+}
+
+func (r *Record) GetStr(col string) ([]byte, bool) {
+	v, ok := r.values[col]
+	return v.Str, ok
+}
+
+func (r *Record) GetInt64(col string) (int64, bool) {
+	v, ok := r.values[col]
+	return v.I64, ok
+}
+
+type TableDef struct {
+	Name    string
+	Columns []Column
+	PKeys   int    // first N columns are primary key
+	Prefix  uint32 // auto-assigned for key prefixing
+}
+
+func NewTableDef(name string, pkeys []Column, keys []Column, prefix uint32) TableDef {
+	return TableDef{
+		Name:    name,
+		Columns: append(pkeys, keys...),
+		PKeys:   len(pkeys),
+		Prefix:  prefix,
+	}
+
+}
+
+func (t *TableDef) GetColumn(name string) (Column, bool) {
+	for _, v := range t.Columns {
+		if v.Name == name {
+			return v, true
+		}
+	}
+	return Column{}, false
+}
+
+func (t *TableDef) GetColumnIndex(name string) int {
+	for i, v := range t.Columns {
+		if v.Name == name {
+			return i
+		}
+	}
+	return 0
+}
+
+func (t *TableDef) GetPrimaryKeys() []Column {
+	return t.Columns[:t.PKeys]
+}
+
+func (t *TableDef) GetNonPrimaryKeys() []Column {
+	return t.Columns[t.PKeys:]
+}
+
+func (t *TableDef) EncodeKey(record Record) ([]byte, error) {
+	b := []byte{}
+	b = binary.LittleEndian.AppendUint32(b, t.Prefix)
+	for _, v := range t.GetPrimaryKeys() {
+		val, ok := record.Get(v.Name)
+		if !ok {
+			return nil, fmt.Errorf("missing pk key: %s", v.Name)
+		}
+		b = append(b, val.encode()...)
+
+	}
+	return b, nil
+
+}
+
+func (t *TableDef) EncodeValue(record Record) ([]byte, error) {
+	b := []byte{}
+	for _, v := range t.GetNonPrimaryKeys() {
+		val, ok := record.Get(v.Name)
+		if !ok {
+			return nil, fmt.Errorf("missing pk key: %s", v.Name)
+		}
+		b = append(b, val.encode()...)
+
+	}
+	return b, nil
+}
+
+func (T *TableDef) DecodeValues(b []byte) ([]Value, error) {
+	result := []Value{}
+	for {
+		if len(b) < 4 {
+			break
+		}
+		valType := DataType(binary.LittleEndian.Uint32(b[:4]))
+		b = b[4:] // Move pointer forward
+		switch valType {
+		case TYPE_BYTES:
+			length := binary.LittleEndian.Uint16(b[:2])
+			b = b[2:] // Move pointer forward
+			result = append(result, NewBytes(b[:length]))
+			b = b[length:] // Move pointer forward
+
+		case TYPE_INT64:
+			fmt.Printf("%+v", b)
+			result = append(result, NewInt64(int64(binary.LittleEndian.Uint64(b[:8]))))
+			b = b[8:] // Move pointer forward
+
+		}
+
+	}
+	return result, nil
+}
---
+++ types_test.go
@@ -0,0 +1,167 @@
+package database
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestNewRecord(t *testing.T) {
+	record := NewRecord()
+	record.AddInt64("int", 1337)
+	record.AddStr("str", []byte("string"))
+
+	str, found := record.GetStr("str")
+	if !found {
+		t.Fatal("should found str but got false")
+	}
+	if !bytes.Equal(str, []byte("string")) {
+		t.Fatalf("should get str got %s, should %s", string(str), "string")
+	}
+
+	int, found := record.GetInt64("int")
+	if !found {
+		t.Fatal("should found str but got false")
+	}
+	if int != 1337 {
+		t.Fatalf("should get int got %d, should %d", int, 1337)
+	}
+}
+
+func TestEncodeKeySinglePK(t *testing.T) {
+	pkeys := Column{
+		Name: "pk",
+		Type: TYPE_BYTES,
+	}
+	other := Column{
+		Name: "k",
+		Type: TYPE_BYTES,
+	}
+	table := NewTableDef("table-name", []Column{pkeys}, []Column{other}, 1)
+
+	record := NewRecord()
+	record.AddStr("pk", []byte("key"))
+
+	key, err := table.EncodeKey(record)
+	if err != nil {
+		t.Fatalf("should not err")
+	}
+
+	expect := []byte{1, 0, 0, 0, 1, 0, 0, 0, 3, 0, 'k', 'e', 'y'}
+	if !bytes.Equal(expect, key) {
+		t.Fatalf("should get str got %+v, should %+v", key, expect)
+	}
+}
+
+func TestEncodeKeyTwoPK(t *testing.T) {
+	pkeysOne := Column{
+		Name: "pk1",
+		Type: TYPE_BYTES,
+	}
+	pkeysTwo := Column{
+		Name: "pk2",
+		Type: TYPE_INT64,
+	}
+	other := Column{
+		Name: "k",
+		Type: TYPE_INT64,
+	}
+	table := NewTableDef("table-name", []Column{pkeysOne, pkeysTwo}, []Column{other}, 1)
+
+	record := NewRecord()
+	record.AddStr("pk1", []byte("one"))
+	record.AddInt64("pk2", 100)
+
+	key, err := table.EncodeKey(record)
+	if err != nil {
+		t.Fatalf("should not err")
+	}
+
+	expect := []byte{1, 0, 0, 0, 1, 0, 0, 0, 3, 0, 'o', 'n', 'e', 2, 0, 0, 0, 100, 0, 0, 0, 0, 0, 0, 0}
+	if !bytes.Equal(expect, key) {
+		t.Fatalf("should get str got %+v, should %+v", key, expect)
+	}
+
+}
+
+func TestEncodeValue(t *testing.T) {
+	pkeys := Column{
+		Name: "pk",
+		Type: TYPE_BYTES,
+	}
+	other := Column{
+		Name: "k",
+		Type: TYPE_BYTES,
+	}
+	table := NewTableDef("table-name", []Column{pkeys}, []Column{other}, 1)
+
+	record := NewRecord()
+	record.AddStr("pk", []byte("key"))
+	record.AddStr("k", []byte("value"))
+
+	key, err := table.EncodeValue(record)
+	if err != nil {
+		t.Fatalf("should not err")
+	}
+	expect := []byte{1, 0, 0, 0, 5, 0, 'v', 'a', 'l', 'u', 'e'}
+	if !bytes.Equal(expect, key) {
+		t.Fatalf("should get str got %+v, should %+v", key, expect)
+	}
+	values, err := table.DecodeValues(expect)
+	if err != nil {
+		t.Fatalf("should not err")
+	}
+	if values[0].Type != TYPE_BYTES {
+		t.Fatalf("wrong type")
+	}
+	expect = []byte{'v', 'a', 'l', 'u', 'e'}
+	if !bytes.Equal(values[0].Str, expect) {
+		t.Fatalf("should get str got %+v, should %+v", values[0].Str, expect)
+	}
+}
+
+func TestEncodeTwoValue(t *testing.T) {
+	pkeys := Column{
+		Name: "pk",
+		Type: TYPE_BYTES,
+	}
+	keyOne := Column{
+		Name: "k1",
+		Type: TYPE_BYTES,
+	}
+	keyTwo := Column{
+		Name: "k2",
+		Type: TYPE_INT64,
+	}
+	table := NewTableDef("table-name", []Column{pkeys}, []Column{keyOne, keyTwo}, 1)
+
+	record := NewRecord()
+	record.AddStr("k1", []byte("one"))
+	record.AddInt64("k2", 100)
+
+	key, err := table.EncodeValue(record)
+	if err != nil {
+		t.Fatalf("should not err")
+	}
+	expect := []byte{1, 0, 0, 0, 3, 0, 'o', 'n', 'e', 2, 0, 0, 0, 100, 0, 0, 0, 0, 0, 0, 0}
+	if !bytes.Equal(expect, key) {
+		t.Fatalf("should get str got %+v, should %+v", key, expect)
+	}
+	values, err := table.DecodeValues(expect)
+	if err != nil {
+		t.Fatalf("should not err")
+	}
+	if values[0].Type != TYPE_BYTES {
+		t.Fatalf("wrong type")
+	}
+	expect = []byte{'o', 'n', 'e'}
+	if !bytes.Equal(values[0].Str, expect) {
+		t.Fatalf("should get str got %+v, should %+v", values[0].Str, expect)
+	}
+
+	if values[1].Type != TYPE_INT64 {
+		t.Fatalf("wrong type")
+	}
+	if values[1].I64 != 100 {
+		t.Fatalf("should get int got %+v, should %+v", values[1].I64, 100)
+	}
+}
